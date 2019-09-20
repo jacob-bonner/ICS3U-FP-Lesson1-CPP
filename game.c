0aded4e49dedfc439cab273d09e60937ab2be24a
Ubuntu <ubuntu@ip-172-31-90-158.ec2.internal>
2019-09-20 18:18:46 +0000

    Initial commit

---
+++ game.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 Jacob Bonner All rights reserved.
+//
+// Created by: Jacob Bonner
+// Created on: September 2019
+// This program displays "Hello, World!" for the GameBoy
+
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main() {
+    printf("Hello, World!");
+
+    return 0;
+}
